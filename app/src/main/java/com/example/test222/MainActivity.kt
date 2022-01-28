aa6490c49d95b9446072ba79947fa5ef65e68773
Malz <43366200+donghyun128@users.noreply.github.com>
2022-01-28 12:55:38 +0900

    first

---
+++ app/src/main/java/com/example/test222/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.test222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alarm_detail)
+    }
+}
